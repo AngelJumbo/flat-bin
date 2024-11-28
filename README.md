91116c3106d8afaa528cf40ad6f07f26f758241a
AngelJumbo <anarjumb@protonmail.com>
2024-11-28 07:24:32 -0500

    install script

--- README.md
+++ README.md
@@ -4,13 +4,12 @@ Script that generates launchers for flatpaks in a bin folder, so they can be use
 ## Installation
 
 ```
-git clone https://github.com/AngelJumbo/flat-bin
-cd flat-bin
-sudo cp ./flat-bin /usr/local/bin/flat-bin
+
+curl -fsSL https://raw.githubusercontent.com/AngelJumbo/flat-bin/main/install.sh | bash
 
 ```
 
 ## Usage
-By default the script generates the launchers in $HOME/.local/bin/ so that folder should be part of your PATH.
+The script generates the launchers in $HOME/.local/bin/ so that folder should be part of your PATH.
 
 You should run this script every time that you install a flatpak app.
--- install.sh
+++ install.sh
@@ -6,12 +6,16 @@ BIN_DIR="$HOME/.local/bin"
 
 mkdir -p "$BIN_DIR"
 
+BINARY_URL="https://raw.githubusercontent.com/AngelJumbo/flat-bin/main/flat-bin"
+
 echo "Downloading flat-bin..."
-curl -L https://github.com/AngelJumbo/flat-bin/releases/latest/download/flat-bin -o "$BIN_DIR/flat-bin"
+curl -fL "$BINARY_URL" -o "$BIN_DIR/flat-bin" || {
+  echo "Error: Unable to download flat-bin. Check the URL or your network connection."
+  exit 1
+}
 
 chmod +x "$BIN_DIR/flat-bin"
 
 echo "Installation complete."
 echo "Make sure that ~/.local/bin is part of your PATH to use flat-bin."
-echo "You can check this by running: export PATH=\$HOME/.local/bin:\$PATH"
 
