a1b0f2ea974f11fb301406d86338ac76e2c260d4
AngelJumbo <anarjumb@protonmail.com>
2024-11-28 07:02:05 -0500

    install script

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ echo "Installing flat-bin..."
 
 BIN_DIR="/usr/local/bin"
 
-curl -L https://github.com/AngelJumbo/flat-bin/releases/latest/download/flat-bin -o "$BIN_DIR/flat-bin"
+echo "Downloading flat-bin..."
+sudo curl -L https://github.com/AngelJumbo/flat-bin/releases/latest/download/flat-bin -o "$BIN_DIR/flat-bin"
 
-chmod +x "$BIN_DIR/flat-bin"
+echo "Setting permissions..."
+sudo chmod +x "$BIN_DIR/flat-bin"
 
 if command -v flat-bin &>/dev/null; then
   echo "flat-bin installed successfully!"
